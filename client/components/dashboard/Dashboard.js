dc1696a117b66c86f85a828e2f735d0819715018
unknown <akinwumiadekanmi2@gmail.com>
2023-02-13 21:45:05 +0100

    another commit

--- client/components/dashboard/Dashboard.js
+++ client/components/dashboard/Dashboard.js
@@ -7,7 +7,7 @@ export default function Dashboard() {
   const [show, setShow] = useState(false);
   return (
     <Fragment>
-      <div className="w-full h-[90vh] lg:h-full flex items-center justify-center bg-orange-200">
+      <div className="w-full h-[90vh] lg:h-full flex items-center justify-center pb-10 bg-orange-200">
         <div className="mobile h-[700px] bg-orange-600 flex flex-col space-y-6">
           <Balance setShow={setShow} />
           <History />
