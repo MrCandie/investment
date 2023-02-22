63bca371d9bd9c8831c7a9ed5561c9b5487e3aa6
unknown <akinwumiadekanmi2@gmail.com>
2023-02-22 09:56:28 +0100

    another commit

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = require("./app");
 
-dotenv.config({ path: "./config.env" });
+dotenv.config();
 
 const db = process.env.DATABASE;
 
